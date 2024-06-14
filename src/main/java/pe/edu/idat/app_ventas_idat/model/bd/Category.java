1ab789618a8db8e017d7a4bd07e00a9859fe40e3
grisel15naye <ramirezventuranayeli@gmail.com>
2024-06-13 20:39:46 -0500

    terminado

---
+++ src/main/java/pe/edu/idat/app_ventas_idat/model/bd/Category.java
@@ -0,0 +1,18 @@
+package pe.edu.idat.app_ventas_idat.model.bd;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table (name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer categoryid;
+    private String categoryname;
+    private String description;
+
+}
---
+++ src/main/java/pe/edu/idat/app_ventas_idat/model/bd/Customer.java
@@ -0,0 +1,14 @@
+package pe.edu.idat.app_ventas_idat.model.bd;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Customer")
+public class Customer {
+
+}
